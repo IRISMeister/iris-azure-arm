c47e80c9bcddf6b84fcc2f01803833775d8be200
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2021-08-03 18:55:59 +0900

    wip

---
+++ iris/iris-standalone-server-ubuntu/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+rg=IRIS-Group
+
+az group delete --name $rg --yes
+az group create --name $rg --location "Japan East"
+
+az deployment group create \
+  --name ExampleDeployment \
+  --resource-group $rg \
+  --template-uri "https://raw.githubusercontent.com/IRISMeister/iris-azure-arm/main/iris/iris-standalone-server-ubuntu/azuredeploy.json" \
+  --parameters @azuredeploy.parameters.json
